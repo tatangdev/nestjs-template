5119ef6bf51a56e20937f1d7dc0181ffeecdc0bb
Tatang <tatangdev@gmail.com>
2026-04-27 09:35:39 +0700

    feat: cleanup app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { WebResponseExceptionFilter } from './common/web-response.filter';
 
 @Module({
   imports: [CatsModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     { provide: APP_PIPE, useClass: ZodValidationPipe },
     { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
     { provide: APP_FILTER, useClass: WebResponseExceptionFilter },
